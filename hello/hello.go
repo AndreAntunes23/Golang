b1ce32f108ef74a36f89f90a8844b571233a0a09
AndreAntunes <andre.antunes@ipremios.com>
2022-10-10 11:49:56 -0300

    aula sobre requisição web, utilizando for para ocntinuar dentro da verificação

--- hello/hello.go
+++ hello/hello.go
@@ -2,32 +2,36 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
+	for {
+		exibeMenu()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conhecço este comando")
-		os.Exit(-1)
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conhecço este comando")
+			os.Exit(-1)
+		}
 	}
 }
 
 func exibeIntroducao() {
 	nome := "André"
-	versao := 1.1
+	versao := 1.2
 	fmt.Println("Olá, Sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
@@ -48,7 +52,14 @@ func leComando() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://www.alura.com.br"
-	// resp, err := http.Get(site)
+	// site := "https://www.alura.com.br"
+	site := "https://random-status-code.herokuapp.com/"
+	resp, _ := http.Get(site)
+	// fmt.Println(resp)
 
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
+	}
 }
